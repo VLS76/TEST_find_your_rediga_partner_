0546f96488edb6a8d6d47f0f9f11f9af63977105
VLS76 <vicentelopezs@gmail.com>
2025-06-05 13:21:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+Quiero hacer una pagina web de test, de prueba, y que para lo que quiero hacer, que sea gratuita.
+
+
+El objetivo es que al seleccionar diferentes campos me muestre una serie de Personas que tienen esos campos en común, y la visualización debería ser en forma de nube  o de red, para que sea más atractiva.
+
+
+Los campos estarían representados en la pantalla como pestañas desplegables, situadas a la izquierda de la pantalla, y habrían tantos pestañas como campos. Los campos son: 
+
+1) Especie. Representa el tipo de especie con la que la persona trabaja.
+
+2) Dispositivos. Son los despositivos con los que habitualmente trabajan.
+
+3) Estudio. En que tipo de estudio trabajan.
+
+4) Proyectos. Los proyectos con los que ha trabajado.
+
+
+Al seleccionar un campo, se abriría un desplegable con una serie de indicadores, los cuales pueden seleccionarse desde ninguno a todos. Estos son los indicadores para cada uno de los campos:
+
+
+Campo "Especie":
+
+a) Ovina
+
+b) Caprina
+
+c) Vacuna
+
+d) Porcina
+
+e) Avícola
+
+f) cunícula
+
+
+Campo "Dispositivo"
+
+a) Drones
+
+b) RFID
+
+c) Collares
+
+d) Cámaras de visión
+
+e) IA
+
+f) Alimentadores automáticos
+
+g) Básculas
+
+h) Sensores acústicos
+
+i) Sensores de movimiento
+
+j) Vallados virtuales
+
+
+Campo "Estudio"
+
+a) Comportamiento alimenticio
+
+b) Comportamiento social
+
+c) Manejo
+
+d) Nutrición
+
+e) Salud
+
+
+Campo "Proyecto"
+
+a) Project1
+
+b) Project2
+
+c) Project3
+
+d) Project4
+
+
+Teniendo todos estos indicadores, cada Persona seleccionaría los campos con las que trabaja, de forma que esta información estaría almacenada en la web y mediante la interacción del usuario, éste seleccionaría los indicadores más deseados de cada campo y la página web mostraría qué personas tienen en común alguno de esos indicadores.
+
+
+Puedes hacerme esta página con algun ejemplo (por ejemplo con 5 personas cuyos indicadores son los que tú quieras)? me das el link de acceso? Quiero que se muestren las Personas que tienen algún indicador de los seleccionados, no es necesario que tenga todos los de la selección realizada por el usuario, independientemente de los campos. Además, quiero que se visualice en forma de red de nodos, donde se vean las conexiones. Puedes hacer que el botón de "aplicar filtro" no esté, y se vaya actualizando a medida que el usuario va seleccionando / deseleccionando indicadores?
